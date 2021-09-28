Fix the normalization

conditional_DCGAN.py
@@ -115,7 +115,7 @@ dataloader = torch.utils.data.DataLoader(
                    transform=transforms.Compose([
                        transforms.Resize(args.img_size),
                        transforms.ToTensor(),
-                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                       transforms.Normalize((0.5), (0.5))
                    ])),
     batch_size=args.batch_size, shuffle=True, drop_last=True)
 print('the data is ok')